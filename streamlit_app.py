8d10063834dd73e35743b694062f1b6385031a75
pravinkhadakkar <88227521+pravinkhadakkar@users.noreply.github.com>
2024-01-08 22:46:05 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,14 +21,23 @@ fruit_to_shows= my_fruit_list.loc [fruits_selected]
 streamlit.dataframe(fruit_to_shows)
 # new section to display response from fruityvise respnse
 streamlit.header("Fruityvice Fruit Advice!")
+# create a funciton 
+def get_fruityvice_data (this_fruit_choice):
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+return fruityvice_normalized
+
 try: 
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information")
   else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      streamlit.dataframe(fruityvice_normalized)
+    back_from_function=get_fruityvice_data (fruit_choice)
+    streamlit.dataframe (back_from_function)
+   #   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+   #  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   #   streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
     streamlit.error ()
 streamlit.stop()
