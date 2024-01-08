9810c383beabf2abce800895346724456c6de00a
pravinkhadakkar <88227521+pravinkhadakkar@users.noreply.github.com>
2024-01-08 21:49:42 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,17 +19,29 @@ fruit_to_shows= my_fruit_list.loc [fruits_selected]
 # Display the table on the page.
 # streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruit_to_shows)
+# new section to display response from fruityvise respnse
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try : 
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error ( "Please select a fruit to get information")
+    else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice )
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error ()
+streamlit.stop()
+#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+#streamlit.write('The user entered ', fruit_choice)
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice )
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice )
 # streamlit.text(fruityvice_response.json())
 # write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
-streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
